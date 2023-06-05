f1b416948d20292f6e7b3cbb79e7b0d085248924
Davi Rodrigues <davirodrigues232003@gmail.com>
2023-06-04 23:45:15 -0300

    register js

--- codigo/tip score/script/register.js
+++ codigo/tip score/script/register.js
@@ -1,50 +1,37 @@
-console.log("Importando a lib register.js");
-function validateForm() {
-    const name = document.querySelector(".name").value;
-    const email = document.querySelector(".email").value;
-    const password = document.querySelector(".password").value;
-    const confirmPassword = document.querySelector(".confirmpassword").value;
-    const phone = document.querySelector(".phone").value;
-  
-    if (name.trim() === "") {
-      alert("Por favor, preencha o campo Nome");
-      return false;
-    }
-  
-    else if (email.trim() === "") {
-      alert("Por favor, preencha o campo Email");
-      return false;
-    }
-  
-    else if (password.trim() === "") {
-      alert("Por favor, preencha o campo Senha");
-      return false;
-    }
-  
-    else if (confirmPassword.trim() === "") {
-      alert("Por favor, preencha o campo Confirmar senha");
-      return false;
-    }
-  
-    else if (password !== confirmPassword) {
-      alert("As senhas não correspondem");
-      return false;
-    }
-  
-    else if (phone.trim() === "") {
-      alert("Por favor, preencha o campo Telefone");
-      return false;
-    }
-  
-    // Armazena as informações no Local Storage
-    const userData = {
-      name: name,
-      email: email,
-      password: password,
-      phone: phone
-    };
-  
-    localStorage.setItem("userData", JSON.stringify(userData));
-  
-    return true;
-  }
+ // Obter o elemento de formulário
+ const form = document.getElementById('signup-form');
+
+ // Listen for form submission
+ form.addEventListener('submit', function (event) {
+   event.preventDefault(); // Prevent the form from submitting
+
+   // Get the form inputs
+   const user = document.getElementById('user').value;
+   const email = document.getElementById('email').value;
+   const password = document.getElementById('password').value;
+   const confirmPassword = document.querySelector('.confirm-password').value;
+   const celular = document.getElementById('celular').value;
+
+   // Validação
+   if (password !== confirmPassword) {
+     alert('As senhas não coincidem');
+     return;
+   }
+
+   //  objeto com os dados do formulário
+   const formData = {
+     user,
+     email,
+     password,
+     celular
+   };
+
+   // Salvar os dados do formulário no armazenamento local
+   localStorage.setItem('formData', JSON.stringify(formData));
+
+   //Limpa as entradas do formulário
+   form.reset();
+
+   // Mensagem formuçário
+   alert('Formulário enviado com sucesso!');
+ });
