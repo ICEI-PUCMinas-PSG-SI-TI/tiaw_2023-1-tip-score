29b301b8848ea2eceaa59e8d572cbf1ca134317d
Davi Rodrigues <davirodrigues232003@gmail.com>
2023-06-13 21:28:22 -0300

    LOCAL STORAGE

--- codigo/tip score/script/register.js
+++ codigo/tip score/script/register.js
@@ -1,37 +1,42 @@
 // Obter o elemento do formulário
- const form = document.getElementById('signup-form');
+const form = document.getElementById('signup-form');
 
- 
- form.addEventListener('submit', function (event) {
-   event.preventDefault(); 
+form.addEventListener('submit', function (event) {
+  event.preventDefault(); 
 
-    // Obter os inputs do formulário
-   const user = document.getElementById('user').value;
-   const email = document.getElementById('email').value;
-   const password = document.getElementById('password').value;
-   const confirmPassword = document.querySelector('.confirm-password').value;
-   const celular = document.getElementById('celular').value;
+  // Obter os inputs do formulário
+  const user = document.getElementById('user').value;
+  const email = document.getElementById('email').value;
+  const password = document.getElementById('password').value;
+  const confirmPassword = document.querySelector('.confirm-password').value;
+  const celular = document.getElementById('celular').value;
 
-   // Validação
-   if (password !== confirmPassword) {
-     alert('As senhas não coincidem');
-     return;
-   }
+  // Validação
+  if (password !== confirmPassword) {
+    alert('As senhas não coincidem');
+    return;
+  }
 
-   //  objeto com os dados do formulário
-   const formData = {
-     user,
-     email,
-     password,
-     celular
-   };
+  // Obter a array de cadastros do Local Storage
+  let cadastros = JSON.parse(localStorage.getItem('cadastros')) || [];
 
-   // Salvar os dados do formulário no armazenamento local
-   localStorage.setItem('formData', JSON.stringify(formData));
+  // Objeto com os dados do formulário
+  const formData = {
+    user,
+    email,
+    password,
+    celular
+  };
 
-   //Limpa as entradas do formulário
-   form.reset();
+  // Adicionar o objeto à array de cadastros
+  cadastros.push(formData);
 
-   // Mensagem formulário
-   alert('Formulário enviado com sucesso!');
- });
+  // Salvar a array de cadastros atualizada no Local Storage
+  localStorage.setItem('cadastros', JSON.stringify(cadastros));
+
+  // Limpar as entradas do formulário
+  form.reset();
+
+  // Mensagem de sucesso
+  alert('Formulário enviado com sucesso!');
+});
