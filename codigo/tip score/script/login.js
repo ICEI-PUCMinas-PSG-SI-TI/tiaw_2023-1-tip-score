682fcb5b181db56e13d15133709eff86515aeb1e
viitorcoelho <vitorcoelho5004@gmail.com>
2023-06-22 11:18:59 -0300

    Login-e-Página-de-Perfil-funcionando

---
+++ codigo/tip score/script/login.js
@@ -0,0 +1,36 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const form = document.getElementById('login-form');
+  
+    form.addEventListener('submit', function(event) {
+      event.preventDefault();
+  
+      // Obter os inputs do formulário
+      const email = document.getElementById('email').value;
+      const password = document.getElementById('password').value;
+  
+      // Obter a array de cadastros do Local Storage
+      let cadastros = JSON.parse(localStorage.getItem('cadastros')) || [];
+  
+      // Verificar se o email e a senha correspondem a algum cadastro
+      const matchingUser = cadastros.find(
+        user => user.email === email && user.password === password
+      );
+  
+      if (matchingUser) {
+        // Login bem-sucedido, redirecionar para a página de perfil
+        // Armazenar os dados do usuário no localStorage para acessá-los na página de perfil
+        localStorage.setItem('currentUser', JSON.stringify(matchingUser));
+        window.location.href = 'perfil.html';
+        localStorage.setItem('isNewUser', true);
+
+      } else {
+        // Login inválido, exibir mensagem de erro
+        alert('Email ou senha inválidos');
+      }
+  
+      // Limpar as entradas do formulário
+      form.reset();
+    });
+});
+
+  
