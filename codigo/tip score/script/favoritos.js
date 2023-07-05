f66f3f388242692910f42966a3570acc255ce29d
Davi Rodrigues <davirodrigues232003@gmail.com>
2023-07-05 14:57:02 -0300

    favoritos

---
+++ codigo/tip score/script/favoritos.js
@@ -0,0 +1,19 @@
+function toggleStar(element) {
+    element.classList.toggle('star-filled');
+    var league = element.parentNode.innerText.trim();
+    var favorites = JSON.parse(localStorage.getItem('favorites')) || [];
+  
+    if (element.classList.contains('star-filled')) {
+      if (!favorites.includes(league)) {
+        favorites.push(league);
+      }
+    } else {
+      var index = favorites.indexOf(league);
+      if (index !== -1) {
+        favorites.splice(index, 1);
+      }
+    }
+  
+    localStorage.setItem('favorites', JSON.stringify(favorites));
+  }
+  
